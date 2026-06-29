b01099068f4e65a99bddea733202b980be8fe083
arishatehri-ui <arishatehri@gmail.com>
2026-06-29 20:14:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-document.getElementById("startBtn").onclick = function(){
+const button = document.getElementById("startBtn");
 
-alert("Welcome ❤️");
+button.addEventListener("click", () => {
 
-};
+    document.getElementById("title").innerHTML =
+    "Happy Birthday, Rafid ❤️";
+
+    document.getElementById("message").innerHTML =
+    "This little website was made especially for you.";
+
+    button.innerHTML = "Continue ➜";
+
+});
