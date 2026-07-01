83e3be37cc15b03f6063f5add0007bd1eb3a13b8
fromarii <arishatehri@gmail.com>
2026-07-02 01:25:53 +0600

    Update cake.js

--- cake.js
+++ cake.js
@@ -1,6 +1,18 @@
+const music = document.getElementById("bgMusic");
+
+music.volume = 0.5;
+
+const savedTime = localStorage.getItem("musicTime");
+
+if (savedTime) {
+    music.currentTime = parseFloat(savedTime);
+}
+
+music.play().catch(() => {});
+
 const candles = document.getElementById("candles");
 
-for(let i = 0; i < 20; i++){
+for (let i = 0; i < 20; i++) {
 
     candles.innerHTML += `
         <div class="candle">
@@ -10,26 +22,24 @@ for(let i = 0; i < 20; i++){
 
 }
 
-document.getElementById("wishBtn").addEventListener("click",()=>{
+document.getElementById("wishBtn").addEventListener("click", () => {
 
-    document.querySelectorAll(".flame").forEach(flame=>{
+    document.querySelectorAll(".flame").forEach(flame => {
 
-        flame.style.opacity="0";
-        flame.style.transform="translateX(-50%) scale(0)";
+        flame.style.opacity = "0";
+        flame.style.transform = "translateX(-50%) scale(0)";
 
     });
 
-    document.getElementById("message").style.opacity="1";
+    createConfetti();
 
-    // Optional: go to next page after 4 seconds
-    // setTimeout(() => {
-    //     window.location.href = "nextpage.html";
-    // }, 4000);
+    document.getElementById("message").style.opacity = "1";
 
 });
-function createConfetti(){
 
-    const colors=[
+function createConfetti() {
+
+    const colors = [
         "#ff5f6d",
         "#ffd166",
         "#06d6a0",
@@ -39,26 +49,27 @@ function createConfetti(){
         "#f28482"
     ];
 
-    for(let i=0;i<180;i++){
+    for (let i = 0; i < 180; i++) {
 
-        const confetti=document.createElement("div");
-        confetti.className="confetti";
+        const confetti = document.createElement("div");
+        confetti.className = "confetti";
 
-        confetti.style.left=Math.random()*100+"vw";
-        confetti.style.background=
-            colors[Math.floor(Math.random()*colors.length)];
+        confetti.style.left = Math.random() * 100 + "vw";
 
-        confetti.style.animationDuration=
-            3+Math.random()*3+"s";
+        confetti.style.background =
+            colors[Math.floor(Math.random() * colors.length)];
 
-        confetti.style.transform=
-            `rotate(${Math.random()*360}deg)`;
+        confetti.style.animationDuration =
+            3 + Math.random() * 3 + "s";
+
+        confetti.style.transform =
+            `rotate(${Math.random() * 360}deg)`;
 
         document.body.appendChild(confetti);
 
-        setTimeout(()=>{
+        setTimeout(() => {
             confetti.remove();
-        },6000);
+        }, 6000);
 
     }
 
